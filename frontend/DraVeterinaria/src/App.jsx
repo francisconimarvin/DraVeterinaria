44c72d3e9694d5c8662612e0591f5bd0ace504ea
francisconimarvin <francisconimarvin@gmail.com>
2025-09-28 21:16:21 -0300

    Importado y creado esqueleto de router-dom

--- frontend/DraVeterinaria/src/App.jsx
+++ frontend/DraVeterinaria/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Services from './components/Services.jsx'
 
 
@@ -8,6 +9,12 @@ const App = () => {
     <div className='w-full overflow-hidden'>
       <Header/>
       <Services/>
+      <Router>
+        <Routes>
+          <Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
