7ef6eb155e4ab828e2474867d0e7158533199cfb
francisconimarvin <francisconimarvin@gmail.com>
2025-09-28 21:11:47 -0300

    Añadido react-router-dom

--- frontend/DraVeterinaria/src/components/Services.jsx
+++ frontend/DraVeterinaria/src/components/Services.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Services = () => {
   return (
-    <div className='grid grid-cols-4 gap-8 place-items-center mb-20'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 place-items-center mb-20'>
         <div className='transition-transform transform duration-500 hover:scale-105'>
             <img className='h-40 w-40 cursor-pointer' src={assets.logoInyectadora} alt="" />
             <p className='flex flex-col items-center font-semibold text-2xl'>Vacunas</p>
