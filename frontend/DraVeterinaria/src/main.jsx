ce45d1fb2efa59308a37e766c68bd88686a871b8
francisconimarvin <francisconimarvin@gmail.com>
2025-10-04 19:56:46 -0300

    Arreglo de Auth0 imports y uso.

--- frontend/DraVeterinaria/src/main.jsx
+++ frontend/DraVeterinaria/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Auth0Provider
+      domain="dev-3log5xxbg3n3rw3h.us.auth0.com"
+      clientId="TrgumAiEAT5ZnJsnSK8DKA0EkZNj6mJr"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </StrictMode>
 )
