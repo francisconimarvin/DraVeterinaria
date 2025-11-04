235b25a57f35af8e9bba4dd935fedcce78dbb887
sevelez <se.velez@duocuc.cl>
2025-11-04 00:47:39 -0300

    ...

---
+++ backend/Scheduling/src/main/java/com/draveterinaria/cl/model/Servicio.java
@@ -0,0 +1,10 @@
+package com.draveterinaria.cl.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+
